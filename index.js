21cd801602ef45aed4b96d7e142c72221b8a03b5
joseferrer <jcfb90@gmail.com>
2021-12-07 12:28:40 -0500

    Cambios => Se agregar la generacion de un qr con post

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
-const { response } = require("express");
+const { response, json } = require("express");
 const express = require("express");
+const qr = require("qrcode");
 const app = new express();
 const fs = require("fs.extra");
 const multer = require("multer");
 const uploadFolder = __dirname + "/uploads/";
 const tempFolder = __dirname + '/tmp/';
 
+
 const upload = multer({dest: uploadFolder});
 const port = 3000;
 
+app.use(express.json()); // JSON 
+
+app.use(express.urlencoded({ // URLENCODE
+  extended: true
+}));
 
 app.get("/", (req, res) => {
     res.json({
@@ -32,6 +39,34 @@ app.post('/file_upload', upload.single('single-file'), function(request, respons
       return;
     }
     response.json({success:true, message: 'File uploaded successfully', fileName: fileName});
+  });
+});
+
+// Generate QR code 
+app.post("/generate", (req, res) => {
+  
+  const nombre = req.body.nombre;
+  const identificacion = req.body.identificacion;
+
+  const obj = JSON.stringify({
+    "nombre": nombre,
+    "identificacion": identificacion
+  });
+
+  console.log(obj);
+
+  
+
+  qr.toDataURL(obj, (err, code)=> {
+    if(err){ 
+      return console.log("Error en el qr => ", err)
+    } else {
+      res.status(200).json({
+        success: true,
+        value: code
+      })
+      console.log(code);
+    }
   });
 });
 
