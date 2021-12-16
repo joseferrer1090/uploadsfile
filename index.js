d4811c9fdd5d4642bda7c2bb008f8f091dc7753e
joseferrer <jcfb90@gmail.com>
2021-12-16 10:59:19 -0500

    Cambios => se agrega la primera parte del login con su validacion en Yup y con su middleware de redireccionamiento

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const { response, json } = require("express");
 const express = require("express");
-const qr = require("qrcode");
 const app = new express();
+const qr = require("qrcode");
 const fs = require("fs.extra");
+const Yup = require("yup");
 const multer = require("multer");
 const uploadFolder = __dirname + "/uploads/";
 const tempFolder = __dirname + '/tmp/';
@@ -52,11 +53,10 @@ app.post("/generate", (req, res) => {
     "nombre": nombre,
     "identificacion": identificacion
   });
-
+  
   console.log(obj);
 
-  
-
+  // Este es el que genera la imagen
   qr.toDataURL(obj, (err, code)=> {
     if(err){ 
       return console.log("Error en el qr => ", err)
@@ -68,6 +68,51 @@ app.post("/generate", (req, res) => {
       console.log(code);
     }
   });
+
+  // Este Es el que imprime en la console
+  qr.toString(obj, (err, code) => {
+    if(err){
+      return console.log("Error => ", err);
+    } else {
+      console.log(code)
+    }
+  })
+});
+
+// Login con OAUTH 2.0 y passportjs --> intentando <--
+
+// Implementado Yup para validar el login del usuar
+const login_post = Yup.object({
+  body: Yup.object({
+    username: Yup.string().required(),
+    password: Yup.string().required(),
+    grand_type: Yup.string().required()
+  })
+});
+
+// Se crea un middleware para capturar la data del POST del login
+const validate = (schema) => async (req, res, next) => {
+  try {
+    await schema.validate({
+      body: req.body
+    });
+    console.log("Entre por este lado");
+    return next();
+  } catch (error) {
+    return res.status(500).json({
+      type: error.type,
+      message: error.message
+    });
+  }
+}
+
+
+app.post("/login", validate(login_post), (req, res) => {
+  console.log("res => ", res);
+  console.log("req => ", req.body);
+  return res.json({
+    message: req.body
+  })
 });
 
 
